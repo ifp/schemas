07bf0ce830f56a683e1e7933ab818996ba7d4a76
Ingram Monk <ingram.monk@gmail.com>
2016-09-13 12:51:59 +0100

    added $as_array to loadFixture

--- src/Fixtures/Loader.php
+++ src/Fixtures/Loader.php
@@ -4,12 +4,12 @@ namespace FPAPI\Schemas\Fixtures;
 
 class Loader
 {
-    public function loadFixture($fixture_file)
+    public function loadFixture($fixture_file, $as_array = true)
     {
         $file_path = dirname(__FILE__) . '../../json/fixtures/' . $fixture_file .'.json';
 
         if (file_exists($file_path)) {
-            return json_decode(file_get_contents(dirname(__FILE__) . '../../json/fixtures/' . $fixture_file .'.json'), true);
+            return json_decode(file_get_contents(dirname(__FILE__) . '../../json/fixtures/' . $fixture_file .'.json'), $as_array);
         }
 
         return [];
