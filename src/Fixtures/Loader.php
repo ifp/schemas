69d343185faf2f0e93621df209d5136bcc1bbbda
Ingram Monk <ingram.monk@gmail.com>
2016-09-14 12:35:26 +0100

    fixed Loader

--- src/Fixtures/Loader.php
+++ src/Fixtures/Loader.php
@@ -6,10 +6,10 @@ class Loader
 {
     public function loadFixture($fixture_file, $as_array = true)
     {
-        $file_path = dirname(__FILE__) . '../../json/fixtures/' . $fixture_file .'.json';
+        $file_path = dirname(__FILE__) . '/../../json/fixtures/' . $fixture_file .'.json';
 
         if (file_exists($file_path)) {
-            return json_decode(file_get_contents(dirname(__FILE__) . '../../json/fixtures/' . $fixture_file .'.json'), $as_array);
+            return json_decode(file_get_contents($file_path), $as_array);
         }
 
         return [];
