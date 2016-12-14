0cec112029fac20fbe8e886d547aff1e8291c631
Ingram Monk <ingram.monk@gmail.com>
2016-12-14 15:01:14 +0000

    fixing loadFixture file path

--- src/Fixtures/Loader.php
+++ src/Fixtures/Loader.php
@@ -6,7 +6,7 @@ class Loader
 {
     public function loadFixture($fixture_file, $as_array = true)
     {
-        $file_path = dirname(__FILE__) . '/../../json/fixtures/' . $fixture_file .'.json';
+        $file_path = realpath(dirname(__FILE__) . '/../../json/fixtures/') . '/' . $fixture_file .'.json';
 
         if (file_exists($file_path)) {
             return json_decode(file_get_contents($file_path), $as_array);
